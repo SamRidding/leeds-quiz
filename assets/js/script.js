997ba6d78984aa6ded104975b4a30461e3f5176b
vfg-95 <sam.ridding@hotmail.co.uk>
2022-01-04 17:19:13 +0000

    Add variables for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
-const points = 1
-const totalQuestions = 10
-
+/* Quiz Progress Variables */
 const question = document.getElementById('question');
 const options = document.getElementsByClassName('option-text');
 const questionValue = document.getElementById('questionValue');
 const scoreValue = document.getElementById('score');
 const barProgress = document.getElementById('barProgress');
+
+/* Score & Question Variables */
+const points = 1
+const totalQuestions = 10
+let score = 0
+let questionPool;
+let currentQuestion;
+let questionsAnswered;
+let availableQuestions;
+
+
+
+
+
+
 
 
