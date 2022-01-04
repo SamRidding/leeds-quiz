71993b6c070beaac2b3dd9d541833610ec075d28
vfg-95 <sam.ridding@hotmail.co.uk>
2022-01-04 17:18:45 +0000

    Add variables for quiz

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const points = 1
+const totalQuestions = 10
+
+const question = document.getElementById('question');
+const options = document.getElementsByClassName('option-text');
+const questionValue = document.getElementById('questionValue');
+const scoreValue = document.getElementById('score');
+const barProgress = document.getElementById('barProgress');
+
+
